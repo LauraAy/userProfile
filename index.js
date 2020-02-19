a6c54af70dc6b415c1063d6efc3decc6b01fb7b4
LauraAy <laura.ay1564@gmail.com>
2020-02-18 22:29:11 -0800

    adding markdown formatting to readme file

--- index.js
+++ index.js
@@ -22,37 +22,79 @@ prompt([
   },
   {
     type: 'input',
-    name: 'toc',
-    message: 'What is the Table of contents for your project?'
+    name: 'installation',
+    message: 'Tell us the install command for this project.'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Tell us about usage.'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Tell us about your license'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Who is contributing?'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What command will run tests?'
   }
 
 ])
 
 
-.then(({githubName, projectTitle, description, toc}) => {
+  .then(({ githubName, projectTitle, description, installation, usage, license, contributing, tests }) => {
 
-  axios.get(`https://api.github.com/users/${githubName}`)
-    .then((response) => {
-      console.log(response.data.avatar_url);
+    axios.get(`https://api.github.com/users/${githubName}`)
+      .then(({ data }) => {
+        console.log(data.avatar_url);
 
-      console.log(projectTitle, description, toc)
+        console.log(projectTitle, description)
 
-      const html = `
-    <img src=${response.data.avatar_url}>
-    <p> ${response.data.email}</p>
-    <h1>${projectTitle}</h1>
-    <p>${description}</p>
-    <p>${toc}</p>
-  `
-      fs.writeFile('profile.html', html, e => e ? console.log(e) : null)
-    }, (error) => {
-      console.log(error);
-    });
+const readme = `
+##Project Title
+${projectTitle}
+## Description
+${description}
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
+## Installation
+To install necessary dependencies, run the following command:
+\`\`\`
+${installation}
+\`\`\`
+## Usage
+${usage}
+${license}
+  
+## Contributing
+${contributing}
+## Tests
+To run tests, run the following command:
+\`\`\`
+${tests}
+\`\`\`
+## Questions
+<img src="${data.avatar_url}" alt="avatar" style="border-radius: 16px" width="30" />
+If you have any questions about the repo, open an issue or contact [${data.github}](${data.url}) directly at ${data.email}.
 
+`
+    fs.writeFile('profile.md', readme, e => e ? console.log(e) : null) 
   })
   .catch(e => console.error(e))
-
+})
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,78 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
+${renderLicenseBadge(data.license, data.github, data.title)}
+## Description
+${data.description}
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:
+\`\`\`
+${data.installation}
+\`\`\`
+## Usage
+${data.usage}
+${renderLicenseSection(data.license)}
+  
+## Contributing
+${data.contributing}
+## Tests
+To run tests, run the following command:
+\`\`\`
+${data.test}
+\`\`\`
+## Questions
+<img src="${data.avatar_url}" alt="avatar" style="border-radius: 16px" width="30" />
+If you have any questions about the repo, open an issue or contact [${data.github}](${data.url}) directly at ${data.email}.
 `;
 }
+
+
+
+
+
+
+# ${ projectTitle }
+## Description
+${ description }
+## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+## Installation
+To install necessary dependencies, run the following command:
+\`\`\`
+${installation}
+\`\`\`
+## Usage
+${usage}
+${renderLicenseSection(license)}
+  
+## Contributing
+${contributing}
+## Tests
+To run tests, run the following command:
+\`\`\`
+${tests}
+\`\`\`
+## Questions
+<img src="${data.avatar_url}" alt="avatar" style="border-radius: 16px" width="30" />
+If you have any questions about the repo, open an issue or contact [${data.github}](${data.url}) directly at ${data.email}.
+
+
+
+
+
+
+
 
 module.exports = generateMarkdown;
